238f664db722c93f32b25932746a622efda71972
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-10 19:48:23 +0300

    Update SolutionForNamingVariableTask.cs

--- VariableNames/SolutionForNamingVariableTask.cs
+++ VariableNames/SolutionForNamingVariableTask.cs
@@ -1,2 +1,12 @@
 nodes - findedNodesWithRightValue (finded_nodes_with_right_value) 
-// Список (List в C#) найденных в связном списке нод с заданным значеним.
+// Список (List в C#) найденных в связном списке нод с заданным значеним
+bytes - stringKeyBytes (key_string_bytes)
+// Массив байтов строки-ключа
+slot - slotIndexFromHashFunction (slot_index_from_hash_function)
+// Индекс слота возращенного хеш-функцией
+result - resultOfStringComparation (result_of_string_comparation)
+// Число, знак которого указывает результат сравнения строк
+i - indexOfSuffixBegining (index_of_suffix_begining)
+// Индекс начала суффикса в строке
+result - sumOfTwoLinkedList (sum_of_two_linked_list)
+// результат суммирования двух связных списков
