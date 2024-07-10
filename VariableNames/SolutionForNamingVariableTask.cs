3ec4782934a83402cc1784aadd307881e2286e14
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-11 02:29:16 +0300

    Update SolutionForNamingVariableTask.cs

--- VariableNames/SolutionForNamingVariableTask.cs
+++ VariableNames/SolutionForNamingVariableTask.cs
@@ -1,6 +1,6 @@
 nodes - findedNodesWithRightValue (finded_nodes_with_right_value) 
 // Список (List в C#) найденных в связном списке нод с заданным значеним
-bytes - stringKeyBytes (key_string_bytes)
+bytes - stringKeyBytes (string_key__bytes)
 // Массив байтов строки-ключа
 slot - slotIndexFromHashFunction (slot_index_from_hash_function)
 // Индекс слота возращенного хеш-функцией
@@ -9,4 +9,16 @@ result - resultOfStringComparation (result_of_string_comparation)
 i - indexOfSuffixBegining (index_of_suffix_begining)
 // Индекс начала суффикса в строке
 result - sumOfTwoLinkedList (sum_of_two_linked_list)
-// результат суммирования двух связных списков
+// Результат суммирования двух связных списков
+result - resultsOfOpearations (results_of_operations)
+// Стек с результатами выполнения арифметических операций
+operations - allowedOperations (allowed_opearation)
+// Массив с допустимыми арифметическими оперциями
+temp - bufferOfDigits (buffer_of_digits)
+// Строка с цифрами, которые постепенно складываюся в число
+discount - dicountPercents (discount_percents)
+// Скидка в процентах, а не её абсолютное значение
+password - newPasswordValue (new_password_value)
+// Новое значение пароля
+strength - baseStrengthOfPlayer (base_strength_of_player)
+// Базовое значение характеристики "Сила" у игрового персонажа
