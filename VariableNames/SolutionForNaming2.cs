9338c050c9988ebb5d5dc72d8bbcd12e28703d74
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-12 23:09:28 +0300

    Update SolutionForNaming2.cs

--- VariableNames/SolutionForNaming2.cs
+++ VariableNames/SolutionForNaming2.cs
@@ -1,7 +1,34 @@
 6.1
 6.2
 6.3
-  
+public class Node
+   {
+     public int value;
+     public Node next, prev;
+    ...
+   }
+// Аргументы ноды двухсвязного списка
+
+public class LinkedList2
+   {
+     public Node head;
+     public Node tail;
+     private int count;
+     ...
+      
+     public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+     {
+       _nodeToInsert.next = _nodeAfter.next;
+       _nodeToInsert.prev = _nodeAfter;
+       _nodeToInsert.next.prev = _nodeToInsert;
+       _nodeAfter.next = _nodeToInsert;
+       ++count;
+     }
+      ...
+    }
+// Переменные в методе InsertAfter
+
+
 6.4
 avg - averageNeighborValue (average_neighbor_value)
 // среднее арифметическое соседних элементов массива
