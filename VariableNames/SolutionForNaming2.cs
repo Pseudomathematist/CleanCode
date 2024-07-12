91f140dcc520d085070b69c1398ba96bc665f4a4
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-13 00:03:46 +0300

    Update SolutionForNaming2.cs

--- VariableNames/SolutionForNaming2.cs
+++ VariableNames/SolutionForNaming2.cs
@@ -1,5 +1,19 @@
 6.1
+avarageValue - averageNeighborValue (average_neighbor_value)
+// среднее арифметическое соседних элементов массива, а не какого-то множества значений
+amount - paymentAmountInCents (payment_amount_in_Cents)
+// сумма платежа в центах, переменная в функции округления цены до целых долларов
+userCredentials - userPassword (user_password)
+// пароль пользователя, а не какие-то его общие данные
+array - sortedArray (sorted_array)
+// упорядоченный массив, а не какой-то массив
+createdButtonWithText - createdButton (created_button)
+// Созданная кнопка вообще, а не конкрено созданная кнопка с текстом
 6.2
+Назвать структуру stableSortedStructure
+Назвать переменую HashIndex
+Назвать пространство имен HighOrdedFuncions
+Назвать функцию EvaluteLikeFiniteAutomata
 6.3
 public class Node
    {
@@ -9,25 +23,31 @@ public class Node
    }
 // Аргументы ноды двухсвязного списка
 
-public class LinkedList2
+public static bool IsTrueBracketsSeq(string str)
    {
-     public Node head;
-     public Node tail;
-     private int count;
-     ...
-      
-     public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
-     {
-       _nodeToInsert.next = _nodeAfter.next;
-       _nodeToInsert.prev = _nodeAfter;
-       _nodeToInsert.next.prev = _nodeToInsert;
-       _nodeAfter.next = _nodeToInsert;
-       ++count;
-     }
-      ...
-    }
-// Переменные в методе InsertAfter
+       Stack<char> leftBrackets = new Stack<char>();
+       foreach (char i in str)
+       {
+         if (i == '(') leftBrackets.Push(i);
+         else if (i == ')') 
+         { 
+           if (leftBrackets.Size() == 0) return false;
+           leftBrackets.Pop();
+         }
+       }
+       return leftBrackets.Size() == 0;
+   }
+// Переменная в методе IsTrueBracketsSeq
 
+public static bool IsPalindrome(string str)
+    {
+       Deque<char> strDeque = new Deque<char>();
+       foreach (var i in str) strDeque.AddFront(i);
+       for (int i = strDeque.Size(); i > 1; --i) 
+         if (strDeque.RemoveFront() != strDeque.RemoveTail()) return false;
+       return true;
+    }
+// Переменная метода IsPalindrome
 
 6.4
 avg - averageNeighborValue (average_neighbor_value)
