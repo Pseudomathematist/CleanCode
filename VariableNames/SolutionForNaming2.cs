8fa45b4c72b23569154e880b0e19d50efbeb3e6b
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-12 22:51:44 +0300

    Create SolutionForNaming2.cs

---
+++ VariableNames/SolutionForNaming2.cs
@@ -0,0 +1,18 @@
+6.1
+6.2
+6.3
+  
+6.4
+avg - averageNeighborValue (average_neighbor_value)
+// среднее арифметическое соседних элементов массива
+vec1 - firstVector (first_vector)
+// Первый параметр функции, один из двух vector'ов
+temp - substrBetweenPoints (substr_between_points)
+// Подстрока, ограниченная двумя точками
+i - indexOfSubstrStart (index_of_substr_start)
+// Индекс начала подстроки
+number - twoNumbers (two_numbers)
+// массив содержащий ровно два числа
+  
+
+  
