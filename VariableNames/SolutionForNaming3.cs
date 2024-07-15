829296d533b174cf664b3760d5fcecba4f07dadd
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-15 17:19:41 +0300

    Update SolutionForNaming3.cs

--- VariableNames/SolutionForNaming3.cs
+++ VariableNames/SolutionForNaming3.cs
@@ -1,1 +1,12 @@
+7.1. 
 magicAllowed - isMagicEnabled (is_magic_enabled)
+eventTriggered - isEventActive
+inCombat - isInCombat
+notHasItem - hasItem
+notEnoughSkillPoints - areSkillPointsEnough
+7.2.
+7.3. Проверьте, правильно ли вы даёте имена индексам циклов. Попробуйте найти случай, когда вместо i j k нагляднее использовать более выразительное имя.
+
+7.4. Попробуйте найти в своих решениях два-три случая, когда можно использовать пары имён - антонимы.
+
+7.5
