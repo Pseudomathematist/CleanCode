4bee80d26a2b10d6af3549f44d395724b0d280dd
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-15 20:27:18 +0300

    Update SolutionForNaming3.cs

--- VariableNames/SolutionForNaming3.cs
+++ VariableNames/SolutionForNaming3.cs
@@ -56,4 +56,79 @@ bool FindElementInMatrix(int[,] matrix, int targetElement)
 
 7.4. 
 
-7.5
+7.5.
+temp - bufferOfDigits (buffer_of_digits)
+
+public static int PostfixOperations(string str)
+      {
+          var operationsDictionary = new Dictionary<string, Operation>
+          {
+              {"+", (x, y) => x + y},
+              {"*", (x, y) => x * y},
+              {"-", (x, y) => x - y},
+              {"/", (x, y) => x / y}
+          };
+          Stack<string> buffer = new Stack<string>();
+          Stack<int> result = new Stack<int>();
+          string temp = "";
+          char[] operations = {'+', '*', '-', '/', '='};
+          for (int i = str.Length - 1; i >= 0; --i)
+          {
+            if (Char.IsDigit(str[i]))
+            {
+                temp = str[i] + temp;
+                if (i == 0) buffer.Push(temp);
+            }
+            else if (str[i] == ' ' && temp != "")
+            {
+                buffer.Push(temp);
+                temp = "";
+            }
+            else foreach (char j in operations)
+            {
+                if (str[i] == j)
+                {
+                    buffer.Push(str[i].ToString());
+                    break;
+                }
+            }
+          }
+          for (int i = buffer.Size(); i > 0; --i)
+          {
+              temp = buffer.Pop();
+              int number;
+              if (int.TryParse(temp, out number)) result.Push(number);
+              else if (temp == "=") return result.Pop();
+              else
+              {
+                  int numberSecond = result.Pop(), numberFirst = result.Pop();
+                  result.Push(operationsDictionary[temp](numberFirst, numberSecond));
+              }
+          }
+          return default(int);
+           
+	    }
+
+
+  
+Можно убрать отдельную переменную node
+public Node Find(int _value)
+     {
+       Node node = head;
+       while (node != null)
+       {
+         if (node.value == _value) return node;
+         node = node.next;
+       }
+       return null;
+     }
+
+Вот так
+public Node Find(int _value)
+     {
+       for(Node node = head; node != null; node = node.next)
+       {
+         if (node.value == _value) return node;
+       }
+       return null;
+     }
