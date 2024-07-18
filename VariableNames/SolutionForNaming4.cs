9133376886f3c6a4e7355cefb0cf839d6fb86637
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-18 20:33:39 +0300

    Update SolutionForNaming4.cs

--- VariableNames/SolutionForNaming4.cs
+++ VariableNames/SolutionForNaming4.cs
@@ -6,6 +6,19 @@ str - InputString (string_input)
 // Входная строка, отличная от выходной строки
 num1 - firstNumber (first_number)
 // Первое число из двух
-get - getUserStatus (get_user_status)
-// 
-  
+getted - gettedUserStatus (get_user_status)
+// Переменная со статусом пользователя 
+total - totalPrice (total_price)
+// Полная стоимость товара
+user - userId (user_id)
+// идентификатор пользователя
+array - users (users)
+// массив представителей класса User
+health - healthPoints (health_points)
+// здоровье в очках, а не в процентах
+distance - distanceKm (distance_km)
+// расстояние в километрах
+temperature - temperatureC (temperature_с)
+// температура в градусах по цельсию
+timeData - timeOfDay (time_of_day)
+// время суток, а не DateTime 
