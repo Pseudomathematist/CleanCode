d5c77260e1e98ef57a590577927f9e7a14cba921
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-18 19:26:15 +0300

    Create SolutionForNaming4.cs

---
+++ VariableNames/SolutionForNaming4.cs
@@ -0,0 +1,11 @@
+sumOfTwoLinkedList - twoLinkedListSum (two_linked_List_sum)
+// Результат суммирования двух связных списков
+stringKeyBytes - keyBytesString (key_bytes_spring)
+// Массив байтов строки-ключа
+str - InputString (string_input)
+// Входная строка, отличная от выходной строки
+num1 - firstNumber (first_number)
+// Первое число из двух
+get - getUserStatus (get_user_status)
+// 
+  
