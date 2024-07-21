04d9d2979eabd34664547c813d51a89eafe7da21
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-21 19:57:22 +0300

    Update SolutionForNaming.cs

--- VariableNames/SolutionForNaming.cs
+++ VariableNames/SolutionForNaming.cs
@@ -1,6 +1,10 @@
-7.1
+3.1
 CustomerData - Customer
 PaymentDataHandler - Payment
 ReportGenerator - Report
 DocumentProcessor - Document
 OrderManager - Order
+3.2
+FetchData - Get
+ReceiveInfo - Get
+
