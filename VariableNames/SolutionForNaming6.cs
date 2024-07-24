3deee183c624a6ede1f0bab6400e4cd7232a9ff1
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-24 16:06:28 +0300

    Update SolutionForNaming6.cs

--- VariableNames/SolutionForNaming6.cs
+++ VariableNames/SolutionForNaming6.cs
@@ -2,3 +2,7 @@ IncreasePower1 - IncreasePowerNatural
 // Повысить силы персонажа естественным путем, через тренировки
 IncreasePower2 - IncreasePowerArtifical
 // Повысить силы персонажа искусственным путем, через приём препаратов
+GoToPoint - WalkToPoint
+// Идти до точки на карте
+GoToPoint - RideToPoint
+// Ехать до точки на карте
