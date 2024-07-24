793a85905bd0f83d1574c6d7c45866cae62477dd
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-24 15:11:51 +0300

    Create SolutionForNaming6.cs

---
+++ VariableNames/SolutionForNaming6.cs
@@ -0,0 +1,4 @@
+IncreasePower1 - IncreasePowerNatural
+// Повысить силы персонажа естественным путем, через тренировки
+IncreasePower2 - IncreasePowerArtifical
+// Повысить силы персонажа искусственным путем, через приём препаратов
