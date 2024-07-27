c39e7f8be72f27ad1322e51f2cdcfd8111a89a72
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-07-27 17:20:55 +0300

    Update SolutionForNaming7.cs

--- VariableNames/SolutionForNaming7.cs
+++ VariableNames/SolutionForNaming7.cs
@@ -21,3 +21,46 @@
     }
 }
 //
+
+public class Character
+{
+    private string name;
+    private int level;
+    private int strength;
+    private int agility;
+    private int intelligence;
+
+    private Character(string name, int level, int strength, int agility, int intelligence)
+    {
+        this.name = name;
+        this.level = level;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    public static Character CreateStandardCharacter(string name)
+    {
+        return new Character(name, 1, 5, 5, 5);
+    }
+
+    public static Character CreateCharacter(string name, int level, int strength, int agility, int intelligence)
+    {
+        return new Character(name, level, strength, agility, intelligence);
+    }
+
+    public static Character CreateClassCharacter(string name, string className)
+    {
+        switch (className)
+        {
+            case "Warrior":
+                return new Character(name, 1, 8, 3, 2);
+            case "Mage":
+                return new Character(name, 1, 2, 3, 8);
+            case "Rogue":
+                return new Character(name, 1, 3, 8, 2);
+            default:
+                return new Character(name, 1, 5, 5, 5);
+        }
+    }
+}
