379d6f596b560db92e5e7d1929990c14f7ca0d03
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-08-11 11:13:52 +0300

    Create SolutionForNaming12.cs

---
+++ VariableNames/SolutionForNaming12.cs
@@ -0,0 +1,8 @@
+public class DynArray<T>
+   {
+     public T [] array;
+     public int count;
+     public int capacity;
+     ...
+   }
+// В реализации динамического массива полк класса count...
