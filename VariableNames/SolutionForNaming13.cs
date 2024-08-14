dff2e9be5c9aabc85e7e1617763e01a92c280a5e
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-08-14 16:06:44 +0300

    Update SolutionForNaming13.cs

--- VariableNames/SolutionForNaming13.cs
+++ VariableNames/SolutionForNaming13.cs
@@ -1,3 +1,12 @@
 inventory = ["Меч", "Щит", "Зелье исцеления"]
 inventory = {"Меч": 1, "Щит": 1, "Зелье исцеления": 3}
 // Использовать словарь вместо массива для хранения предметов в инвентаре.
+int[] timers = new int[5]; // 5 таймеров
+
+
+Dictionary<string, float> timers = new Dictionary<string, float>() 
+{
+    { "AttackCooldown", 0f },
+    { "PowerUpDuration", 10f }
+};
+// Использование словаря для хранения таймеров с названиями
