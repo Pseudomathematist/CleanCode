ad935a0d99eacb4df48287144cca9890d7ea6443
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-08-23 20:31:26 +0300

    Update SolutionForNaming16.cs

--- VariableNames/SolutionForNaming16.cs
+++ VariableNames/SolutionForNaming16.cs
@@ -16,9 +16,13 @@ if (Physics.CheckSphere(transform.position, 1f))
 
 // Создание эффекта взрыва
 GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-
 // Уничтожение эффекта взрыва
 Destroy(explosion, 2f);
+
+// Создание эффекта взрыва на текущем положении объекта и уничтожение его через 2 секунды
+GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+Destroy(explosion, 2f);
+
 
 
 ////////////////////////
