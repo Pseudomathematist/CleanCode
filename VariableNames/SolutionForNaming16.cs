816ee9f1a19b1a195beff47dce95904e62c81e03
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-08-23 20:29:03 +0300

    Update SolutionForNaming16.cs

--- VariableNames/SolutionForNaming16.cs
+++ VariableNames/SolutionForNaming16.cs
@@ -6,6 +6,15 @@ if (Physics.CheckSphere(transform.position, 1f))
     // Применить действие
     ApplyAction();
 }
+
+// Применить действие, если объект сталкивается с другим объектом в радиусе 1 метра
+if (Physics.CheckSphere(transform.position, 1f))
+{
+    ApplyAction();
+}
+
+
+
 ////////////////////////
 Улучшение кода по типу 2
 
